f69312e0ee2a705384cb6dd8d853846e29e2aa7e
Quentin Jeriko <54465409+5WAGGER@users.noreply.github.com>
2026-03-14 20:44:12 -0700

    Mutator that sets data member
    Define the Restaurant class's SetName() mutator that sets data member name to restaurantName, followed by "'s Buffet", and the SetRating() mutator that sets data member rating to restaurantRating.

---
+++ restaurant_reviewer.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+class Restaurant {
+   public:
+      void SetName(string restaurantName);      
+      void SetRating(int restaurantRating);     
+      void Print() const;   
+   private:
+      string name;
+      int rating;
+};
+
+void Restaurant::SetName(string restaurantName) {
+   name = restaurantName + "'s Buffet";
+}
+
+void Restaurant::SetRating(int restaurantRating) {
+   rating = restaurantRating;
+}
+
+
+void Restaurant::Print() const {
+   cout << "Restaurant: " << name << endl;
+   cout << "Rating: " << rating << " stars" << endl;
+}
+
+int main() {
+   Restaurant restaurant;
+   string inputName;
+   int inputRating;
+
+   cin >> inputName;
+   cin >> inputRating;
+   restaurant.SetName(inputName);
+   restaurant.SetRating(inputRating);
+
+   restaurant.Print();
+
+   return 0;
+}
+
+
+//Input should be:
+//Meg's Buffet
